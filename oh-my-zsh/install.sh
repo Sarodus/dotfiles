6c6672af432641fab5050d63c0c2b7b5b6d92980
Sarodus <jordiavellapol@gmail.com>
2017-05-09 16:12:42 +0200

    fix

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -2,4 +2,4 @@ git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-
 rm "$HOME/.oh-my-zsh/oh-my-zsh.sh"
 ln -s "$ZSH/oh-my-zsh/oh-my-zsh.sh" "$HOME/.oh-my-zsh/oh-my-zsh.sh"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
